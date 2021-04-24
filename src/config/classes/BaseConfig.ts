2157df34132587ba71598d778f87ca62c8194d26
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-04-24 08:44:26 +0100

    Remove some old methods that aren’t needed after move to typedjson

--- src/config/classes/BaseConfig.ts
+++ src/config/classes/BaseConfig.ts
@@ -10,13 +10,4 @@ export class Base {
     valid(): boolean {
         return true
     }
-    convert(): void { }
-
-    // use this to see if the class has and values setup or if its an empty class
-    // - means you can give properties default values
-    hasValues(): boolean {
-        const has_val: boolean = Object.values(this).some(v => v !== null && typeof v !== "undefined")
-        core.debug('hasValues: - ' + has_val)
-        return has_val
-    }
 }
