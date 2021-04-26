74abbdf673cbe58767dd0ac7acab14ba0b1f39df
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-04-26 11:35:06 +0100

    Add basic test for main class

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,5 +1,15 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import {Config} from '../src/config'
+import {yaml_to_config} from '../src/yaml/yaml_to_config'
 
-test('place holder', async () => {})
+const sample_dir: string = './__samples__/'
+
+test('working configuration load', async () => {
+    const configuration_file: string = sample_dir + 'config/valid/simple.yml'
+    const configuration: Config = await yaml_to_config(configuration_file)
+
+    expect(configuration.filesystem.directory).toBe('./')
+    expect(configuration.filesystem.follow_symlinks).toBeTruthy()
+})
