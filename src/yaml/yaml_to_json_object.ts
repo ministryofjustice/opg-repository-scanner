31dc81b44ea906432929aed83a0baa32f4b8953c
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-04-26 14:53:18 +0100

    typo in object

--- src/yaml/yaml_to_json_object.ts
+++ src/yaml/yaml_to_json_object.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs'
 // convert a yaml file over to a json object for processing
 export async function yaml_to_json_object(filename: string, encoding = 'utf8'): Promise<Object> {
     return new Promise<Object>(resolve => {
-        const options = {encloding: encoding, flag: 'r'}
+        const options: object = {encoding, flag: 'r'}
         const content = fs.readFileSync(filename, options).toString()
         const json = yaml.load(content) as Object
 
