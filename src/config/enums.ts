18aa60585d1c3bc9a4317d3586db239a431b1fe2
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-04-26 12:02:20 +0100

    Change the value of the parser enum to be easier to use later

--- src/config/enums.ts
+++ src/config/enums.ts
@@ -2,7 +2,7 @@
 // - when a selector finds a result, this enum determines how to process it
 export enum PackageFileParsers {
     none = '',
-    json_key_value_pair = 'json-key-value'
+    json_key_value_pair = 'JsonKeyValuePair'
 }
 
 export enum PackageFileTypes {
