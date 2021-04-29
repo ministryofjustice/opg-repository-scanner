dc3ff509311de2d14e067803868d922eba11c4be
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-04-29 17:23:06 +0100

    Enable these tests

--- __tests__/composer/classes/ComposerManifestHandler.test.ts
+++ __tests__/composer/classes/ComposerManifestHandler.test.ts
@@ -15,69 +15,69 @@ import { Filesystem } from '../../../src/config'
 // base all file scanning on this diretory
 const sample_dir: string = './__samples__/'
 
-test('test', async () => {})
+//test('test', async () => {})
 
-// test('postive: test a composer manifest validates', async () => {
-//     const dir:string = sample_dir + 'app/php/laminas/'
-//     const pattern:string = '**/composer.json'
-//     const filesys:Filesystem = new Filesystem(dir, false)
-//     const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
-//     expect(manifest.valid()).toBeTruthy()
-// })
+test('postive: test a composer manifest validates', async () => {
+    const dir:string = sample_dir + 'app/php/laminas/'
+    const pattern:string = '**/composer.json'
+    const filesys:Filesystem = new Filesystem(dir, false)
+    const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
+    expect(manifest.valid()).toBeTruthy()
+})
 
-// test('negative: test a composer manifest fails', async () => {
-//     const dir:string = sample_dir + 'app/php/laminas/'
-//     const pattern:string = ''
-//     const filesys:Filesystem = new Filesystem(dir, false)
-//     const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
-//     expect(manifest.valid()).toBeFalsy()
-// })
+test('negative: test a composer manifest fails', async () => {
+    const dir:string = sample_dir + 'app/php/laminas/'
+    const pattern:string = ''
+    const filesys:Filesystem = new Filesystem(dir, false)
+    const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
+    expect(manifest.valid()).toBeFalsy()
+})
 
-// test('postive: test a composer manifest handler finds correct files from known dir', async () => {
-//     const dir:string = sample_dir + 'app/php/laminas/'
-//     const pattern:string = '**/composer.json'
-//     const filesys:Filesystem = new Filesystem(dir, false)
-//     const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
-//     const files = await manifest.files()
-//     expect(files.length).toEqual(1)
-// })
+test('postive: test a composer manifest handler finds correct files from known dir', async () => {
+    const dir:string = sample_dir + 'app/php/laminas/'
+    const pattern:string = '**/composer.json'
+    const filesys:Filesystem = new Filesystem(dir, false)
+    const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
+    const files = await manifest.files()
+    expect(files.length).toEqual(1)
+})
 
-// test('negative: test a composer manifest handler finds no files in missing dir', async () => {
-//     const dir:string = sample_dir + 'app/php/laminas-not-there/'
-//     const pattern:string = 'composer.json'
-//     const filesys:Filesystem = new Filesystem(dir, false)
-//     const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
-//     const files = await manifest.files()
-//     expect(files.length).toEqual(0)
-// })
+test('negative: test a composer manifest handler finds no files in missing dir', async () => {
+    const dir:string = sample_dir + 'app/php/laminas-not-there/'
+    const pattern:string = 'composer.json'
+    const filesys:Filesystem = new Filesystem(dir, false)
+    const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
+    const files = await manifest.files()
+    expect(files.length).toEqual(0)
+})
 
-// test('postive: test a composer manifest handler finds packages from file', async () => {
-//     const dir:string = sample_dir + 'app/php/laminas/'
-//     const pattern:string = 'composer.json'
-//     const file:string = dir + pattern
-//     const filesys:Filesystem = new Filesystem(dir, false)
-//     const manifest = new ComposerManifestHandler(filesys, pattern, [ManifestSelectors.Main])
+test('postive: test a composer manifest handler finds packages from file', async () => {
+    const dir:string = sample_dir + 'app/php/laminas/'
+    const pattern:string = 'composer.json'
+    const file:string = dir + pattern
+    const filesys:Filesystem = new Filesystem(dir, false)
+    const manifest = new ComposerManifestHandler(filesys, pattern, [ManifestSelectors.Main])
 
-//     await manifest.process()
-//     const results = await manifest.results()
+    await manifest.process()
+    const results = await manifest.results()
 
-//     expect(results.length).toEqual(3)
-//     // check type
-//     const first = results.pop()
-//     expect(first).toBeInstanceOf(Result)
-//     expect(first?.type).toEqual('composer-json')
-// })
+    expect(results.length).toEqual(3)
+    // check type
+    const first = results.pop()
+    expect(first).toBeInstanceOf(Result)
+    expect(first?.type).toEqual('composer-json')
+})
 
-// test('postive: test a composer manifest handler finds packages from file with multi-selectors', async () => {
-//     const dir:string = sample_dir + 'app/php/laminas/'
-//     const pattern:string = 'composer.json'
-//     const file:string = dir + pattern
-//     const filesys:Filesystem = new Filesystem(dir, false)
-//     const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
+test('postive: test a composer manifest handler finds packages from file with multi-selectors', async () => {
+    const dir:string = sample_dir + 'app/php/laminas/'
+    const pattern:string = 'composer.json'
+    const file:string = dir + pattern
+    const filesys:Filesystem = new Filesystem(dir, false)
+    const manifest = new ComposerManifestHandler(filesys, pattern, ManifestSelectorsArray)
 
-//     await manifest.process()
-//     const results = await manifest.results()
+    await manifest.process()
+    const results = await manifest.results()
 
-//     expect(results.length).toEqual(17)
+    expect(results.length).toEqual(17)
 
-// })
+})
