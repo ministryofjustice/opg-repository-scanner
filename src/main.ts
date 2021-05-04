859002543f2020b8b4f3eae8a114f10c84b2af1d
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-05-04 16:53:50 +0100

    Run and upload artefacts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,39 @@
 import * as core from '@actions/core'
+import * as artifact from '@actions/artifact'
 import {Config} from './config'
 import {yaml_to_config} from './yaml'
 import {ManifestResults} from './manifestresults'
-// import * as glob from '@actions/glob'
 
 async function run(): Promise<void> {
     try {
         // -- Load configuration
         const configuration_file: string = './configuration.yml'
-        core.setOutput('configuration_file_used', configuration_file)
+        core.debug('configuration_file_used: ' + configuration_file)
 
         const configuration: Config = await yaml_to_config(configuration_file)
-        core.setOutput('configuration_file_loaded', true)
+        core.debug('configuration file loaded')
 
-        //core.debug('configuration file loaded: ' + configuration_file)
+        const handler: ManifestResults = new ManifestResults(configuration)
+        core.debug('handler generated with configuration')
 
-        const handler = new ManifestResults(configuration)
-        core.setOutput('handler_loaded', true)
+        core.debug('handler .process() starting')
+        await handler.process()
+        const files = await handler.save()
 
-        // await handler.process()
-        // await handler.save()
+        core.debug(JSON.stringify(files))
+
+        // generate artefact
+        const artifact_name = configuration.artifact?.name ?? ''
+        const artifact_client = artifact.create()
+
+        if(files.length > 0 && artifact_name.length > 0) {
+            core.debug('Generating artefact: ' + artifact)
+            const response = await artifact_client.uploadArtifact(artifact_name, files, __dirname , {
+                continueOnError: false
+            })
+
+        }
+
     } catch (error) {
         core.setFailed(error.message)
     }
