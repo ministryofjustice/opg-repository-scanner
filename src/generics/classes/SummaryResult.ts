251a180bccc84159e7950fa58249c7b3aa8ae748
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-05-07 14:06:59 +0100

    Summary results now converts data over from Result

--- src/generics/classes/SummaryResult.ts
+++ src/generics/classes/SummaryResult.ts
@@ -9,21 +9,31 @@ export class SummaryResult{
     version:string = ''
     tags:string[] = []
 
-
     occurances:object = {}
 
 
-    from_result(result:Result): SummaryResult{
-        this.name = result.name
-        this.tags = result.tags
+    version_from_result(result:Result) {
+        // show the manifest version (where we can) and then track how many other unique versions there are
+        let versions:string[] = []
+        let version:string = ''
+        for(const item of result.occurances) {
+            versions.push(item.version)
+            if(item.type == ManifestType.Manifest && version === '') version = item.version
+        }
+        // filter out duplicates by using set
+        versions = [...new Set([...versions])]
+
+        if( version !== '' && versions.length > 1) this.version = `${version} (+ ${versions.length} others)`
+        else if ( version !== '') this.version = version
+        else if( versions.length > 1 ) {
+            version = versions.shift() ?? ''
+            this.version = `${version} (+ ${versions.length} others)`
+        } else if( versions.length > 0 )  this.version = versions.shift() ?? ''
+
+    }
+
+    occurances_from_result(result:Result) {
         let occurances: Map<string, number> = new Map<string, number>()
-        /* eslint-disable no-console */
-
-        // try and find the manifest version or just use the first occurance
-        const manifest = result.occurances.filter(i => i.type === ManifestType.Manifest).shift()
-        if (typeof manifest !== 'undefined') this.version = manifest.version
-        else this.version = result.occurances.shift()?.version ?? ''
-
         // reduce the result occurences to counters by filepath
         for(const item of result.occurances) {
             const file = item.source
@@ -33,8 +43,25 @@ export class SummaryResult{
             occurances.set(file,  count)
         }
         this.occurances = map_to_object(occurances)
+    }
 
-        /* eslint-enable no-console */
+    tag_updates(result:Result) {
+        let tags = result.tags
+        const first = tags.includes('is:' + ManifestType.Manifest)
+        const third = tags.includes('is:' + ManifestType.Lock)
+
+        if(first) tags.push("is:first-party")
+        else if (third) tags.push("is:third-party")
+
+        tags = [...new Set([...tags])]
+        this.tags = tags
+    }
+
+    from_result(result:Result): SummaryResult{
+        this.name = result.name
+        this.version_from_result(result)
+        this.occurances_from_result(result)
+        this.tag_updates(result)
         return this
     }
 
