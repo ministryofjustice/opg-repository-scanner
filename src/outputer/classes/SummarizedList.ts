d2f3337d15fa9c4521ca8d0a5e5c4c22b4208127
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-05-07 16:53:11 +0100

    Output markdown files as well

--- src/outputer/classes/SummarizedList.ts
+++ src/outputer/classes/SummarizedList.ts
@@ -6,10 +6,9 @@ import { List } from './List';
 
 export class SummarizedList extends List implements IOutputer{
 
-    protected filename = 'list.summary.json'
+    protected filename = 'list.summary'
 
-    write(data:Map<string,any>): string {
-        /* eslint-disable no-console */
+    write(data:Map<string,any>): string[] {
         let packages:Result[] = data.get('packages')
         let summarised:SummaryResult[] = []
         // we do some post processing on the package data by
@@ -19,15 +18,25 @@ export class SummarizedList extends List implements IOutputer{
             let summary = new SummaryResult()
             summarised.push( summary.from_result(pkg) )
         }
-
         data.set('packages', summarised)
-
-        /* eslint-enable no-console */
-
+        // write this out as json
         const obj = map_to_object(data)
         const json_string:string = JSON.stringify(obj)
-        fs.writeFileSync(this.filename, json_string)
-        return this.filename
+        fs.writeFileSync(this.filename + ".json", json_string)
+        // write to markdown as well, no headers so its easer to merge multiple files
+        let markdown = ''
+        // now loop over data and add to rows
+        for (const row of summarised) {
+            const occ = row.occurances_to_string_array( row.sources() ).join('<br>')
+            const tags = row.tags.join(', ')
+            markdown += `| ${row.name} | ${row.version} | ${occ} | ${tags} |\n`
+        }
+        fs.writeFileSync(this.filename + ".md", markdown)
+
+        return [
+            this.filename + '.md',
+            this.filename + ".json"
+        ]
     }
 
 
