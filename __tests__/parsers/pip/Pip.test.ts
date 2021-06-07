0e4883de81cdc7f56b34066500d3fbffc008608e
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-06-07 14:11:44 +0100

    Add a pip file with versioning for the packages

--- __tests__/parsers/pip/Pip.test.ts
+++ __tests__/parsers/pip/Pip.test.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import {PipParser as Pip} from '../../../src/parsers'
 
 const sample_dir: string = './__samples__/app/pip/valid/'
-
+const versioned_sample_dir: string = './__samples__/app/pip/valid-versioned/'
 
 test(`+ Test the tags function`, async () => {
     const comp = new Pip()
@@ -42,4 +42,24 @@ test(`- Test the manifest function returns correctly`, async () => {
 
     expect(found.length).toEqual(7)
 
+})
+
+
+
+test(`+ Test versions are returned correctly`, async () => {
+    const dir = versioned_sample_dir
+    const comp = new Pip().set(
+        'test-repo',
+        dir,
+        ["(vendor/*)"],
+        false
+    )
+    const found = await comp.manifests(Pip.tags.manifest, Pip.filePatterns.manifest)
+
+    let notVersioned = false
+    found.forEach(f => {
+        f.meta.forEach(m => { if(m.version.length <= 0){ notVersioned = true} })
+    })
+
+    expect(notVersioned).toBeFalsy()
 })
