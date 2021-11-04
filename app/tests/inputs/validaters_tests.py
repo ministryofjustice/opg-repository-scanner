829953106e9d5ec8998014cd5d2eaf8120e69c8d
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-11-04 19:01:10 +0000

    some basic validation tests

--- app/tests/inputs/validaters_tests.py
+++ app/tests/inputs/validaters_tests.py
@@ -6,3 +6,48 @@ from inputs.validators import *
 def test_readable_directory_with_invalid_path():
     with pytest.raises(ValueError) as err:
         readable_directory("./not-a-directory")
+
+def test_readable_directory_with_valid_path():
+    dir = "./tests"
+    assert readable_directory(dir) == dir
+
+
+POSSIBLE_STRING_BOOLEANS = [
+        ("FALSE", False),
+        ("False", False),
+        ("FalSE", False),
+        ("false", False),
+        (" false  ", False),
+        ("TRUE", True),
+        ("True", True),
+        ("TruE", True),
+        ("true", True),
+        (" true  ", True),
+        ("", True),
+        ("1", True),
+        (" ", True)
+    ]
+
+@pytest.mark.parametrize('string_value,expected', POSSIBLE_STRING_BOOLEANS)
+def test_string_to_bool_with_false_values(string_value, expected):
+    assert string_to_bool(string_value) == expected
+
+
+
+VALID_JSON_STRINGS = [
+    ("[]", 0),
+    ('[{"test":1}, {"name":"test"}]', 2)
+]
+@pytest.mark.parametrize('valid,length', VALID_JSON_STRINGS)
+def test_json_string_to_list(valid, length):
+    converted = json_string_to_list(valid)
+    assert len(converted) == length
+
+INVALID_JSON_STRINGS = [
+    "",
+    "{}"
+]
+@pytest.mark.parametrize('invalid', INVALID_JSON_STRINGS)
+def test_json_string_to_list(invalid):
+    with pytest.raises(ValueError) as err:
+        json_string_to_list(invalid)
