b985ca19b991bd017604d0affb122cfcf7e96318
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-11-08 13:39:40 +0000

    adding npm parsing and tests

--- app/parsers/npm.py
+++ app/parsers/npm.py
@@ -1,9 +1,69 @@
 from typing import List
 from . import base
+from files import read
+import json
+from pprint import pp
 
 class npm(base):
     """
     """
+
+
+    manifests: dict = {'include': ['**/package.json'], 'exclude': []}
+    locks: dict = {'include': ['**/package-lock.json'], 'exclude': []}
+    tags: dict = {'manifests': ['npm', 'manifest'], 'locks': ['npm' 'lock']}
+
+
+    def parse_manifest(self, file_path:str, packages:list) -> list:
+        """
+        Parse the dependencies and devDependencies section of the
+        package.json file and convert
+
+        """
+        content = read().content(file_path)
+        config = json.loads(content)
+
+        for pkg, ver in config.get('dependencies', {}).items():
+            packages = self.merge_into_list(packages, 'name', self.package_info(
+                            pkg, ver, file_path, None, self.tags['manifests']
+                        ))
+        for pkg, ver in config.get('devDependencies', {}).items():
+            packages = self.merge_into_list(packages, 'name', self.package_info(
+                            pkg, ver, file_path, None, self.tags['manifests']
+                        ))
+        return packages
+
+
+    def parse_lock(self, file_path:str, packages:list) -> list:
+        """
+        Parse the packages section of the package-lock.json file and convert
+
+        """
+        content = read().content(file_path)
+        config = json.loads(content)
+
+        # loop over packages
+        for name, info in config.get('packages', {}).items():
+            # if it has a name, then should be a version field too, so add the package
+            if len(name) > 0:
+                packages = self.merge_into_list(packages, 'name',
+                            self.package_info(name.replace("node_modules/", ""), info['version'], file_path, None, self.tags['locks']
+                        ))
+            # look at the sub items of packages
+            for sub_name, sub_version in info.get('dependencies', {}).items():
+                packages = self.merge_into_list(packages, 'name',
+                            self.package_info(sub_name, sub_version, file_path, None, self.tags['locks']
+                        ))
+        # check dependancies
+        for name, info in config.get('dependencies', {}).items():
+            packages = self.merge_into_list(packages, 'name',
+                            self.package_info(name, info['version'], file_path, None, self.tags['locks']
+                        ))
+        return packages
+
+
+
+
     @staticmethod
     def handles(tool:str) -> bool:
         return (tool in ['npm', '*'])
--- app/tests/parsers/npm_tests.py
+++ app/tests/parsers/npm_tests.py
@@ -5,3 +5,27 @@ from parsers import npm
 def test_npm_handles():
     assert npm.handles('npm') == True
     assert npm.handles('yarn') == False
+
+
+def test_npm_parse_manifests_empty():
+    p = npm()
+    file = "../__samples/parsers/npm/valid/empty/package.json"
+    manifest = p.parse_manifest(file, [])
+    assert manifest == []
+    assert len(manifest) == 0
+
+
+def test_npm_parse_manifests_nuxt():
+    p = npm()
+    file = "../__samples/parsers/npm/valid/nuxt/package.json"
+    manifest = p.parse_manifest(file, [])
+    assert len(manifest) == 2
+
+
+def test_npm_parse_lock_simple():
+    p = npm()
+    file = "../__samples/parsers/npm/valid/simple/package-lock.json"
+    manifest = p.parse_lock(file, [])
+
+    pp(manifest)
+    assert False
