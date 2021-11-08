3b1655f0beae7f041b77149de4b149696e3b57d2
charlesmmarshall <charles.marshall@digital.justice.gov.uk>
2021-11-08 11:57:29 +0000

    Add start of go parsing

--- app/parsers/__init__.py
+++ app/parsers/__init__.py
@@ -1,3 +1,4 @@
 from .base import *
 from .npm import *
 from .pip import *
+from .gomod import *
---
+++ app/parsers/gomod.py
@@ -0,0 +1,77 @@
+from pprint import pp
+from typing import List
+from . import base
+from files import read
+
+class gomod(base):
+    """
+    """
+
+    manifests: dict = {'include': ['**/go.mod'], 'exclude': []}
+    locks: dict = {'include': ['**/go.sum'], 'exclude': []}
+    tags: dict = {'manifests': ['go', 'gomod', 'manifest'], 'locks': ['go', 'gomod' 'lock']}
+
+
+    def parse_manifest(self, file_path:str, packages:list) -> list:
+        """
+        Read manifest file (go.mod) and convert into a list of dicts.
+        Merge that list of dicts pack in to the packages variable passed along.
+
+        """
+        is_a_package = ' v'
+        is_third_party = '// indirect'
+        reader = read()
+        lines = reader.lines(file_path)
+        # reduce the list of lines to just the packages
+        items = list( filter(lambda l: (is_a_package in l), lines) )
+        # run replaces over each item to clean up the entries
+        mapped = list( map(lambda pkg: pkg.replace('\t', '').split(" "), items) )
+
+        for m in mapped:
+            tags = self.tags['manifests']
+            # if this package is marked as indirect, push a tag
+            if is_third_party in m:
+                tags.append('third-party')
+
+            pkg = self.package_info(
+                    m[0].strip(),
+                    m[1] if len(m) > 1 else None,
+                    file_path,
+                    None,
+                    tags
+                )
+            packages = self.merge_into_list(packages, 'name', pkg)
+
+        return packages
+
+
+
+    def parse_lock(self, file_path:str, packages:list) -> list:
+        """
+        Read lock file (go.sum) and convert into a list of dicts.
+        Merge that list of dicts pack in to the packages variable passed along.
+        """
+        is_a_package = ' v'
+        reader = read()
+        lines = reader.lines(file_path)
+        # reduce the list of lines to just the packages
+        items = list( filter(lambda l: (is_a_package in l), lines) )
+        # run replaces over each item to clean up the entries
+        mapped = list( map(lambda pkg: pkg.split(" "), items) )
+
+        for m in mapped:
+            pkg = self.package_info(
+                    m[0].strip(),
+                    m[1].replace("/go.mod", "") if len(m) > 1 else None,
+                    file_path,
+                    None,
+                    self.tags['locks']
+                )
+            packages = self.merge_into_list(packages, 'name', pkg)
+
+        return packages
+
+
+    @staticmethod
+    def handles(tool:str) -> bool:
+        return (tool in ['go', 'gomod', '*'])
